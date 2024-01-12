143f5d5835fcc228435d8aa1c10e32275b94c722
ichbinnahid <nahid.zarbiyev@gmail.com>
2024-01-13 00:12:51 +0400

    create card wrapper component and login page root

---
+++ components/auth/back-button.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+interface BackButtonProps {
+  label: string;
+  href: string;
+}
+
+export const BackButton = ({ label, href }: BackButtonProps) => {
+  return (
+    <Button className="font-normal w-full" variant={"link"} size={"sm"} asChild>
+      <Link href={href}>{label}</Link>
+    </Button>
+  );
+};
