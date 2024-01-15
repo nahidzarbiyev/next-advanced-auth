6725f7b37e0fb5eaba76f9dd2faed114544ab3ab
ichbinnahid <nahid.zarbiyev@gmail.com>
2024-01-16 02:52:22 +0400

    handle middlewares and login functionality

---
+++ routes.ts
@@ -0,0 +1,28 @@
+/**
+ * an array of routes that are accesible to the public
+ * These routes do not require authentication
+ * @type{String[]}
+ */
+
+export const publicRoutes = ["/"];
+/**
+ * an array of routes that are used for authentication
+ * These routes will redirect logged in users to /settings
+ * @type{String[]}
+ */
+
+export const authRoutes = ["/auth/login", "/auth/register"];
+/**
+ * The prefix for API authentication routes
+ * Routes that start with this prefix are used for API authentication purposes
+ * @type{String[]}
+ */
+
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default redirect path after logging in
+ * @type{String[]}
+ */
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
