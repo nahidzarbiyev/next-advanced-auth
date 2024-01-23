a224d4885662809802c5618121284073f32d362d
ichbinnahid <nahid.zarbiyev@gmail.com>
2024-01-23 22:30:30 +0400

    user button handle

---
+++ app/(protected)/settings/layout.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./_components/navbar";
+
+const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full w-full flex flex-col gapp-y-10 items-center justify-center bg-sky-500">
+      <Navbar />
+      {children}
+    </div>
+  );
+};
+
+export default ProtectedLayout;
--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,18 +1,18 @@
-import { auth, signOut } from "@/auth";
+"use client";
 
-const SettingsPage = async () => {
-  const session = await auth();
+import { logout } from "@/actions/logout";
+import { useCurrentUser } from "@/hooks/use-current-user";
+
+const SettingsPage = () => {
+  const user = useCurrentUser();
+  const onClick = () => {
+    logout();
+  };
   return (
-    <div className="">
-      {JSON.stringify(session)}
-      <form
-        action={async () => {
-          "use server";
-          await signOut();
-        }}
-      >
-        <button type="submit">Sign out</button>
-      </form>
+    <div className="bg-whte p-10 rounded-xl bg-white">
+      <button type="submit" onClick={onClick}>
+        Sign out
+      </button>
     </div>
   );
 };
