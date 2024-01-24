68d24b224af3a365eedc4f70b2e06a2be74a6582
ichbinnahid <nahid.zarbiyev@gmail.com>
2024-01-24 19:31:54 +0400

    bugs fixed

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -2,7 +2,7 @@ import Navbar from "./settings/_components/navbar";
 
 const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full w-full flex flex-col gapp-y-10 items-center justify-center bg-sky-500">
+    <div className="h-full w-full flex flex-col gapp-y-10 items-center justify-center bg-[#272323]">
       <Navbar />
       {children}
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const font = Poppins({
 
 export default function Home() {
   return (
-    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+    <main className="flex h-full flex-col items-center justify-center bg-[#272323]">
       <div className="space-y-6 text-center">
         <h1
           className={cn(
@@ -27,7 +27,7 @@ export default function Home() {
           a simple authentication service
         </p>
         <div className="">
-          <LoginButton  asChild>
+          <LoginButton asChild>
             <Button variant={"secondary"} size={"lg"}>
               Sign in
             </Button>
