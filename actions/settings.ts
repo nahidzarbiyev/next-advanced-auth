80d84b6d12dc5d06c9c1b339b5ea9549f1a6d404
ichbinnahid <nahid.zarbiyev@gmail.com>
2024-01-24 19:37:29 +0400

    deployment

--- actions/settings.ts
+++ actions/settings.ts
@@ -7,7 +7,7 @@ import { currentUser } from "@/lib/auth";
 import { generateVerifcationToken } from "@/lib/tokens";
 import { sendVerificationEmail } from "@/lib/mail";
 import { update } from "@/auth";
-import brcypt from "bcryptjs"
+import brcypt from "bcryptjs";
 export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   const user = await currentUser();
   if (!user) {
@@ -20,40 +20,35 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   }
 
   if (user.isOAuth) {
-    values.email = undefined
-    values.password = undefined
-    values.newPassword = undefined
-    values.isTwoFactorEnabled = undefined
+    values.email = undefined;
+    values.password = undefined;
+    values.newPassword = undefined;
+    values.isTwoFactorEnabled = undefined;
   }
-  if (values.email && values.email !==user.email) {
-    const existinguser = await getUserByEmail(values.email)
+  if (values.email && values.email !== user.email) {
+    const existinguser = await getUserByEmail(values.email);
     if (existinguser && existinguser.id !== user.id) {
-      return { error:"Email already in use!"}
-      
+      return { error: "Email already in use!" };
     }
 
-    const verificationToken = await generateVerifcationToken(values.email)
+    const verificationToken = await generateVerifcationToken(values.email);
     await sendVerificationEmail(
       verificationToken.email,
       verificationToken.token
-    )
-    return {success:"Verirication email sent!"}
-    
+    );
+    return { success: "Verirication email sent!" };
   }
   if (values.password && values.newPassword && dbUser.password) {
     const passwordsMatch = await brcypt.compare(
       values.password,
       dbUser.password
-    )
+    );
     if (!passwordsMatch) {
-      return {error:"Incorrect password"}
+      return { error: "Incorrect password" };
     }
-    const hashedPassword = await brcypt.hash(
-      values.newPassword,
-      10
-    )
-    values.password =hashedPassword;
-    values.newPassword = undefined
+    const hashedPassword = await brcypt.hash(values.newPassword, 10);
+    values.password = hashedPassword;
+    values.newPassword = undefined;
   }
   const updateduser = await db.user.update({
     where: {
@@ -64,11 +59,13 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
     },
   });
   update({
-    name:updateduser.name,
-    email:updateduser.email,
-    isTwoFactorEnabled:updateduser.isTwoFactorEnabled,
-    role:updateduser.role,
-    
-  })
+    user: {
+      name: updateduser.name,
+      email: updateduser.email,
+      isTwoFactorEnabled: updateduser.isTwoFactorEnabled,
+      role: updateduser.role,
+    },
+  });
+
   return { success: "Settings Updated!" };
 };
